fc3dae44279a9b5f5c6e5e2642f883164239286f
Dev Dan <developer_daniel@icloud.com>
2025-03-30 20:13:23 -0700

    docs: removed the identity README, and added documentation to the image module.

--- rsky-pds/src/image/mod.rs
+++ rsky-pds/src/image/mod.rs
@@ -3,13 +3,32 @@ use image::io::Reader as ImageReader;
 use image::{guess_format, GenericImageView};
 use std::io::Cursor;
 
+/// A struct representing the structure of an image
+///
+/// # Examples:
+/// ```rust
+/// use rsky_pds::image::ImageInfo; 
+/// let image = ImageInfo {
+///     height: 512,
+///     width: 512,
+///     size: Option::from(1000000u32),
+///     mime: String::from("image/png")};
+/// ```
+///
+/// This struct does not validate input values. Be sure that `height`, `width`, and `size`
+/// are within the range [0, 2^(32)-1], and that mime is given a string.
 pub struct ImageInfo {
+    /// the height of the image
     pub height: u32,
+    /// the width of the image
     pub width: u32,
+    /// the amount of memory (in bytes) the image takes up
     pub size: Option<u32>,
+    /// the media type of the image
     pub mime: String,
 }
 
+/// Returns the mime-type of the file.
 pub async fn mime_type_from_bytes(bytes: Vec<u8>) -> Result<Option<String>> {
     match infer::get(bytes.as_slice()) {
         Some(kind) => Ok(Some(kind.mime_type().to_string())),
@@ -17,6 +36,7 @@ pub async fn mime_type_from_bytes(bytes: Vec<u8>) -> Result<Option<String>> {
     }
 }
 
+/// Returns the full image details
 pub async fn maybe_get_info(bytes: Vec<u8>) -> Result<Option<ImageInfo>> {
     let process_image = || -> Result<Option<ImageInfo>> {
         let img = ImageReader::new(Cursor::new(bytes.clone()))
